01befd9ad11488874c9c21e08230b19e8c8a6f9d
kimjaejoong <wcn07003@naver.com>
2025-10-14 16:24:34 +0900

    [#1] feat(config): corsMappings 추가

--- src/main/java/com/hiresense/config/SwaggerConfig.java
+++ src/main/java/com/hiresense/config/SwaggerConfig.java
@@ -5,9 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
 
     @Bean
     public OpenAPI openAPI() {
