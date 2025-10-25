a15281c4378f42d9442d39ce4073db59dccd786d
kimjaejoong <wcn07003@naver.com>
2025-10-25 20:30:45 +0900

    [#4] feat(job-posting): job-posting 서비스 로직(CRUD) 구현

---
+++ src/main/java/com/hiresense/jobPosting/service/JobPostingService.java
@@ -0,0 +1,67 @@
+package com.hiresense.jobPosting.service;
+
+import com.hiresense.global.error.exception.ResumeNotFoundException;
+import com.hiresense.jobPosting.domain.JobPosting;
+import com.hiresense.jobPosting.dto.request.JobPostingRequest;
+import com.hiresense.jobPosting.dto.request.JobPostingUpdateRequest;
+import com.hiresense.jobPosting.dto.response.JobPostingResponse;
+import com.hiresense.jobPosting.repository.JobPostingRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class JobPostingService {
+    private final JobPostingRepository jobPostingRepository;
+
+    @Transactional
+    public JobPostingResponse create(JobPostingRequest request) {
+        log.info("채용공고 생성을 시작합니다. companyName: {}", request.companyName());
+        JobPosting jobPosting = JobPosting.createJobPosting(request);
+        JobPosting savedJobPosting= jobPostingRepository.save(jobPosting);
+        log.info("채용공고 생성이 완료되었습니다. id: {}", savedJobPosting.getId());
+        return JobPostingResponse.from(savedJobPosting);
+    }
+
+    public JobPostingResponse findById(Long id) {
+        log.info("ID {} 이력서 조회를 시작합니다.", id);
+        JobPosting jobPosting = jobPostingRepository.findById(id)
+                .orElseThrow(ResumeNotFoundException::new);
+        log.info("ID {} 이력서 조회가 완료되었습니다.", id);
+        return JobPostingResponse.from(jobPosting);
+    }
+
+    public List<JobPostingResponse> findAll() {
+        log.info("모든 이력서 조회를 시작합니다.");
+        List<JobPosting> jobPostings = jobPostingRepository.findAll();
+        log.info("총 {}개의 이력서 조회가 완료되었습니다.", jobPostings.size());
+        return jobPostings.stream()
+                .map(JobPostingResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void update(Long id, JobPostingUpdateRequest request) {
+        log.info("ID {} 이력서 수정을 시작합니다.", id);
+        JobPosting jobPosting = jobPostingRepository.findById(id)
+                .orElseThrow(ResumeNotFoundException::new);
+        jobPosting.updateJobPosting(request);
+        log.info("ID {} 이력서 수정이 완료되었습니다.", id);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        log.info("ID {} 이력서 삭제를 시작합니다.", id);
+        JobPosting jobPosting = jobPostingRepository.findById(id)
+                .orElseThrow(ResumeNotFoundException::new);
+        jobPostingRepository.delete(jobPosting);
+        log.info("ID {} 이력서 삭제가 완료되었습니다.", id);
+    }
+}
