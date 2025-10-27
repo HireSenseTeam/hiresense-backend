092ae8e915e282adcee89755e674059345299924
kimjaejoong <wcn07003@naver.com>
2025-10-27 12:50:43 +0900

    [#4] feat(S3): S3 의존성 추가 및 설정

---
+++ src/main/java/com/hiresense/s3/S3Service.java
@@ -0,0 +1,43 @@
+package com.hiresense.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(String key, byte[] content) throws IOException {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(content));
+
+        return s3Client.utilities().getUrl(builder -> builder.bucket(bucket).key(key)).toExternalForm();
+    }
+
+    public void deleteFile(String key) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+}
