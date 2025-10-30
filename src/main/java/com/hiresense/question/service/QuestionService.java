f00acac84b47e2cb2a745872a0bb03488224b1d1
kimjaejoong <wcn07003@naver.com>
2025-10-30 13:47:05 +0900

    [#11] feat(interview): @PostConstruct와 @Transactional의 동시 사용 문제 해결

--- src/main/java/com/hiresense/question/service/QuestionService.java
+++ src/main/java/com/hiresense/question/service/QuestionService.java
@@ -1,15 +1,8 @@
 package com.hiresense.question.service;
 
-import com.hiresense.question.domain.Question;
-import com.hiresense.question.domain.QuestionType;
 import com.hiresense.question.repository.QuestionRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +10,4 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    @Transactional
-    @PostConstruct
-    public void initCommonQuestions() {
-        if (questionRepository.count() == 0) {
-            List<Question> commonQuestions = Arrays.asList(
-                    Question.builder().text("자기소개 부탁드립니다.").type(QuestionType.COMMON).build(),
-                    Question.builder().text("우리 회사에 지원한 동기가 무엇인가요?").type(QuestionType.COMMON).build(),
-                    Question.builder().text("자신의 장점과 단점에 대해 말해주세요.").type(QuestionType.COMMON).build(),
-                    Question.builder().text("자신의 직무 경험에 대해 말해주세요.").type(QuestionType.COMMON).build(),
-                    Question.builder().text("갈등 상황을 해결한 경험이 있나요?").type(QuestionType.COMMON).build(),
-                    Question.builder().text("우리 회사에서 어떤 일을 하고 싶나요?").type(QuestionType.COMMON).build()
-            );
-            questionRepository.saveAll(commonQuestions);
-        }
-    }
 }
