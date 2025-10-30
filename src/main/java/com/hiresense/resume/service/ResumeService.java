8f79e863a8a992fc403cb2cd95680052ead9d79e
kimjaejoong <wcn07003@naver.com>
2025-10-30 12:02:32 +0900

    [#11] feat(S3): 데이터를 자주 읽고 써야하기 때문에 S3에는 저장 X

--- src/main/java/com/hiresense/resume/service/ResumeService.java
+++ src/main/java/com/hiresense/resume/service/ResumeService.java
@@ -1,19 +1,16 @@
 package com.hiresense.resume.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hiresense.global.error.exception.ResumeNotFoundException;
 import com.hiresense.resume.domain.Resume;
 import com.hiresense.resume.dto.request.ResumeRequest;
 import com.hiresense.resume.dto.request.ResumeUpdateRequest;
 import com.hiresense.resume.dto.response.ResumeResponse;
 import com.hiresense.resume.repository.ResumeRepository;
-import com.hiresense.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,15 +21,12 @@ import java.util.stream.Collectors;
 public class ResumeService {
 
     private final ResumeRepository resumeRepository;
-    private final S3Service s3Service;
-    private final ObjectMapper objectMapper;
 
     @Transactional
     public ResumeResponse create(ResumeRequest request) {
         log.info("이력서 생성을 시작합니다. name: {}", request.name());
         Resume resume = Resume.createFrom(request);
         Resume savedResume = resumeRepository.save(resume);
-        uploadResumeToS3(savedResume);
         log.info("이력서 생성이 완료되었습니다. id: {}", savedResume.getId());
         return ResumeResponse.from(savedResume);
     }
@@ -43,7 +37,6 @@ public class ResumeService {
         Resume resume = resumeRepository.findById(id)
             .orElseThrow(ResumeNotFoundException::new);
         resume.updateFrom(request);
-        uploadResumeToS3(resume);
         log.info("ID {} 이력서 수정이 완료되었습니다.", id);
     }
 
@@ -70,16 +63,6 @@ public class ResumeService {
         Resume resume = resumeRepository.findById(id)
             .orElseThrow(ResumeNotFoundException::new);
         resumeRepository.delete(resume);
-        s3Service.deleteFile("resumes/" + id + ".json");
         log.info("ID {} 이력서 삭제가 완료되었습니다.", id);
-    }
-
-    private void uploadResumeToS3(Resume resume) {
-        try {
-            String jsonContent = objectMapper.writeValueAsString(resume);
-            s3Service.uploadFile("resumes/" + resume.getId() + ".json", jsonContent.getBytes());
-        } catch (IOException e) {
-            log.error("S3에 이력서 업로드를 실패했습니다. id: {}", resume.getId(), e);
-        }
     }
 }
