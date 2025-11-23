1e86256f39e9f38b72f4529d7a6788930a4de17e
kimjaejoong <wcn07003@naver.com>
2025-11-23 15:27:43 +0900

    [#19] feat(question): 타입별, 채용공고별, 이력서별 질문 조회 기능 추가

---
+++ src/main/java/com/hiresense/question/dto/response/QuestionResponse.java
@@ -0,0 +1,12 @@
+package com.hiresense.question.dto.response;
+
+import com.hiresense.question.domain.QuestionType;
+
+public record QuestionResponse(
+        Long id,
+        String text,
+        QuestionType type,
+        Long jobPostingId,
+        Long resumeId
+) {
+}
